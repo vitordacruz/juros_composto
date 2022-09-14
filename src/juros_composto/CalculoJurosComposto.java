a602aa75a82ec0fb0ccafe6f09ce8bc1fd3094d5
vitor <vitordacruz@gmail.com>
2022-09-13 21:44:54 -0300

    Project created

---
+++ src/juros_composto/CalculoJurosComposto.java
@@ -0,0 +1,14 @@
+package juros_composto;
+
+import java.math.BigDecimal;
+
+public class CalculoJurosComposto {
+	
+	private static final BigDecimal JUROS = BigDecimal.valueOf(1.15D);
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module juros_composto {
+}
